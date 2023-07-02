e7669760cf70aa3001184682ddb3d0df8bd86f5e
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-07-02 16:19:26 +0000

    [Finish]practical-3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end  
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
