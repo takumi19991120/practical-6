990c6246632ca58a96adfce5f2fd7e0e92657165
takumi19991120 <135395048+takumi19991120@users.noreply.github.com>
2023-07-06 11:52:58 +0000

    [Finish]practical-5

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,9 +26,6 @@ class User < ApplicationRecord
     followings.include?(user)
   end
   
-  
-  
-  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -38,4 +35,21 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+# 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end  
+  
 end
+
